1a06d6c9a8ada96c1140066b7600d251cbc59e37
Konsta <konsta09@gmail.com>
2012-07-14 12:05:53 +0300

    remove some unneeded cm packages from build
    *mini_phone: set default ringtones, use old audio
    *don't copy sample videos
    *remove cmwallpapers, cmupdater and videoeditor

--- config/common.mk
+++ config/common.mk
@@ -111,11 +111,6 @@ PRODUCT_COPY_FILES +=  \
     vendor/cm/proprietary/Term.apk:system/app/Term.apk \
     vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
 
-# Bring in camera effects
-PRODUCT_COPY_FILES +=  \
-    vendor/cm/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/cm/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
-
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
@@ -156,9 +151,7 @@ PRODUCT_PACKAGES += \
     DSPManager \
     libcyanogen-dsp \
     audio_effects.conf \
-    CMWallpapers \
     Apollo \
-    CMUpdater \
     CMFileManager \
     LockClock \
     CMAccount
--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -2,19 +2,16 @@
 $(call inherit-product, vendor/cm/config/common.mk)
 
 # Bring in all audio files
-include frameworks/base/data/sounds/NewAudio.mk
-
-# Include CM audio files
-include vendor/cm/config/cm_audio.mk
+include frameworks/base/data/sounds/OldAudio.mk
 
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=Orion.ogg \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Hassium.ogg
+    ro.config.ringtone=Ring_Digital_02.ogg \
+    ro.config.notification_sound=F1_New_SMS.ogg \
+    ro.config.alarm_alert=Alarm_Beep_03.ogg
 
 PRODUCT_PACKAGES += \
-  Mms
+    Mms
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
